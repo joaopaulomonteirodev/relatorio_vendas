2d2ac4b50200a9d14668e46155286839b8dea6ba
Joao Paulo Monteiro <jpmonteiro64@gmail.com>
2019-11-27 00:05:31 -0300

    Projeto de Relatorio de Vendas em JS

---
+++ database.js
@@ -0,0 +1,71 @@
+var catalogo = [
+    {
+        categoria: "Entretenimento",
+        produtos: [
+            {
+                nome: "Vídeo Games",
+                marcas: [
+                    {
+                        nome: "Nintendo" ,
+                        vendas: [ 10, 30, 50, 70, 100 ]
+                    },
+                    {
+                        nome: "Sony" ,
+                        vendas: [ 70, 100, 50, 10, 30 ]
+                    },
+                ]
+                
+            },
+            {
+                nome: "Celulares",
+                marcas: [
+                    {
+                        nome: "Samsung" ,
+                        vendas: [ 30, 30, 10, 30, 30 ]
+                    },
+                    {
+                        nome: "Apple" ,
+                        vendas: [ 50, 50, 70, 50, 50 ]
+                    },
+                ]
+                
+            },
+            
+        ]
+    },
+    
+    {
+        categoria: "Eletrônicos",
+        produtos: [
+            {
+                nome: "Notebook",
+                marcas: [
+                    {
+                        nome: "Acer" ,
+                        vendas: [ 100, 40, 30, 80, 100 ]
+                    },
+                    {
+                        nome: "Dell" ,
+                        vendas: [ 100, 80, 60, 40, 20 ]
+                    },
+                ]
+                
+            },
+            {
+                nome: "Tablet",
+                marcas: [
+                    {
+                        nome: "Multilaser" ,
+                        vendas: [ 40, 40, 10, 40, 40 ]
+                    },
+                    {
+                        nome: "Philco" ,
+                        vendas: [ 20, 20, 80, 20, 20 ]
+                    },
+                ]
+                
+            },
+            
+        ]
+    },
+];
