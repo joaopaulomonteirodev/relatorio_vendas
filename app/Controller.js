8450e6c9c967ff89ceb7e44f08e30a1ce4b704ee
Joao Paulo Monteiro <jotaplaysson@MacBook-Pro-de-Joao.local>
2019-11-29 13:47:22 -0300

    Remocao de comentario desnecessario

--- app/Controller.js
+++ app/Controller.js
@@ -1,12 +1,5 @@
 class Controller{
 
-  /*
-routes.get('/categorias', Controller.categorias);
-routes.get('/categorias/:categoriaId/produtos', Controller.produtosCategoria);
-routes.get('/categorias/:categoriaId/produtos/:produtoId/marcas', Controller.marcasProdutoCategoria);
-routes.get('/categorias/:categoriaId/produtos/:produtoId/marcas/:marcaID/vendas', Controller.vendasMarcaProdutoCategoria);
-
-  */
   async categorias(req, res){
     res.json({ok: true});
   }
